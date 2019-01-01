f4d7d6409ba821313fd2e44156a58bf44425d580
DanielPantle <dan.pantle.iel@gmx.de>
2019-01-01 21:26:29 +0100

    remove useless files

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const socket = require('socket.io');
+//const path = require('path');
 
+const PORT = process.env.PORT || 4000;
+
+// app setup
+var app = express();
+var server = app.listen(PORT, function() {
+    console.log('listening on port', PORT);
+});
+
+// static files
+app.use(express.static('public'));
+
+// socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('client disconnected');
+    });
+});
+
+function test() {
+    io.emit('time', new Date().toTimeString());
+}
+
+/*
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -19,3 +50,4 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+*/
